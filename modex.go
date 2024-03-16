188a14d772ff7d6bbcb8089339ebc776cde42283
rootVIII <colleyloyejames@gmail.com>
2024-03-16 14:35:51 -0400

    add comments

--- modex.go
+++ modex.go
@@ -2,6 +2,11 @@ package modex
 
 import "fmt"
 
+// semver 1.2.3
+// 1 is a major and possibly breaking change
+// 2 is for backward compatible changes
+// 3 is patch version and should not be for breaking changes to public API
+
 func Combine(val string) string {
 	return fmt.Sprintf("%[1]s - %[1]s", val)
 }
