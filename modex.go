a63dbdf7d3cb6b0f5acb90d205954500f6802286
rootVIII <colleyloyejames@gmail.com>
2024-03-16 14:38:26 -0400

    comments

--- modex.go
+++ modex.go
@@ -2,10 +2,17 @@ package modex
 
 import "fmt"
 
+// Module created via:
+//go mod init github.com/rootVIII/modex
+
 // semver 1.2.3
 // 1 is a major and possibly breaking change
 // 2 is for backward compatible changes
 // 3 is patch version and should not be for breaking changes to public API
+
+// After pushing changes, use git tag to version module
+// git tag v0.1.0
+// git push origin v0.1.0
 
 func Combine(val string) string {
 	return fmt.Sprintf("%[1]s - %[1]s", val)
