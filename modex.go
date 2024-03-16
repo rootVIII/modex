f3492ddd58d2913bdc6688e90031508ec66a97e7
rootVIII <colleyloyejames@gmail.com>
2024-03-16 15:02:01 -0400

    more comments

--- modex.go
+++ modex.go
@@ -19,13 +19,13 @@ import "fmt"
 // In client codebase use go get to install to ~/go/pkg/mod/github.com
 // go get github.com/rootVIII/modex@v0.1.0
 // go get github.com/rootVIII/modex@v0.1.1
+// Module version is now visible in client codebase's go.mod
 
-// Import into client codebase via:
+// To import this module into client codebase via:
 // import "github.com/rootVIII/modex"
 
-// Module version is visible in client codebase's go.mod
-// Run go get if upgrading to new version (go.mod will update)
-// If the module is not longer used, remove unused modules from go.mod via:
+// Run "go get" with version details if upgrading to new version (go.mod will update)
+// If the module is no longer used, remove unused modules from "go.mod" via:
 // go mod tidy
 
 // Combine Uses v0.1.0
